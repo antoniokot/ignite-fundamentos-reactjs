ffadac29d50e66d904398f8b1a92dd628e24a67a
Antonio Hideto Borges Kotsubo <antoniokotsubo@gmail.com>
2024-03-03 12:18:39 -0300

    counting likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import { 
   ThumbsUp,
   Trash
@@ -8,10 +9,19 @@ import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css';
 
+
 export function Comment ({ content, onDeleteComment }) {
+
+  const [likeCount, setLikeCount] = useState(0)
 
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
   }
 
   return (
@@ -42,9 +52,9 @@ export function Comment ({ content, onDeleteComment }) {
 
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
